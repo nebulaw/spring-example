2039f0e912f28cdf2eaf8f6c7972565dafe6ec01
nebulaw <karlo.tsutskiridze@gmail.com>
2023-11-30 23:39:26 +0400

    added aspect oriented programming example

--- src/main/java/com/example/jpaspringdemo/JpaSpringDemoApplication.java
+++ src/main/java/com/example/jpaspringdemo/JpaSpringDemoApplication.java
@@ -1,15 +1,27 @@
 package com.example.jpaspringdemo;
 
+import com.example.jpaspringdemo.aop.TestPojo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class JpaSpringDemoApplication {
+public class JpaSpringDemoApplication implements CommandLineRunner {
+
+  @Autowired
+  private TestPojo testPojo;
 
   public static void main(String[] args) {
     SpringApplication.run(JpaSpringDemoApplication.class, args);
   }
 
+  @Override
+  public void run(String... args) throws Exception {
+    testPojo.test();
+    System.out.println("Test passed");
+    testPojo.testUtil("Karlo");
+  }
 }
---
+++ src/main/java/com/example/jpaspringdemo/aop/Loggable.java
@@ -0,0 +1,13 @@
+package com.example.jpaspringdemo.aop;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE, ElementType.METHOD })
+public @interface Loggable {
+  // TODO: Log request, endpoint, headers, execution time
+}
